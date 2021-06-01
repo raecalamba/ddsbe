c05c8b33124e7b00782024ba29341966f3715051
raecalamba <raecalamba@gmail.com>
2021-06-01 17:46:32 +0800

    First commit to Repo in Github

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -2,27 +2,30 @@
 
 namespace App\Traits;
 use Illuminate\Http\Response;
-trait ApiResponser
-{
+
+trait ApiResponser{
+
  /**
  * Build success response
  * @param string|array $data
  * @param int $code
  * @return Illuminate\Http\JsonResponse
  */
- public function successResponse($data, $code = Response::HTTP_OK)
- {
- return response()->json(['data' => $data], $code);
+
+ public function successResponse($data, $code = Response::HTTP_OK){
+     return response()->json(['data' => $data, 'site' => 2], $code);
  }
+
  /**
  * Build error responses
  * @param string|array $message
  * @param int $code
  * @return Illuminate\Http\JsonResponse
  */
- public function errorResponse($message, $code)
- {
- return response()->json(['error' => $message, 'code' => $code], 
-$code);
- }
+
+ public function errorResponse($message, $code){
+     return response()->json(['error' => $message, 'site' => 2, 'code' => $code], $code);
+    }
 }
+
+?>
